021eb8a25627dd5e91671d15cc5684ee3a3b15d7
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-09 02:03:28 +0530

    setState() with callback function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: 'Redbull', lastName: 'Racing' },
+      name: { firstName: "Redbull", lastName: "Racing" },
     };
   }
 
@@ -17,11 +17,29 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {firstName} {lastName}</p>
+          <p>
+            Hi {firstName} {lastName}
+          </p>
           <button
             onClick={() => {
-              this.setState({ name: { firstName: 'Mercedes', lastName: 'Racing' } });
-              console.log(this.state);
+              // setState with Object
+              // this.setState({
+              //   name: { firstName: "Mercedes", lastName: "Racing" },
+              // });
+              // console.log(this.state);
+
+              // setState with callback as setState behaves asynchronously
+              this.setState(
+                () => {
+                  return {
+                    name: { firstName: "Mercedes", lastName: "Racing" },
+                  };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+
             }}
           >
             Change Name
