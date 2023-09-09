445267b2d0a362e5bea2c4c3e3f7a62b2ab02b44
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-09 13:21:43 +0530

    input field added

--- src/App.js
+++ src/App.js
@@ -10,18 +10,25 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
     this.setState(() => {
       return { monsters: users };
-    }, () => {
-      console.log(this.state);
     });
   }
 
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
