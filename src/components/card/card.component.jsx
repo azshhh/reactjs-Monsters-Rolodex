118449c895f3008ace7bf14dec3464e7e4737418
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-09 14:35:40 +0530

    Adding CSS and Optimization

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,16 +1,17 @@
 import { Component } from "react";
+import "./card.styles.css";
 
 class Card extends Component {
   render() {
-    const { monster } = this.props;
+    const { name, email, id } = this.props.monster;
     return (
-      <div key={monster.id}>
+      <div className="card-container" key={id}>
         <img
-          src={`https://robohash.org/${monster.id}/set=set2&size=180x180`}
-          alt={`monsters ${monster.name}`}
+          src={`https://robohash.org/${id}/set=set2&size=180x180`}
+          alt={`monsters ${name}`}
         />
-        <h1>{monster.name}</h1>
-        <p>{monster.email}</p>
+        <h1>{name}</h1>
+        <p>{email}</p>
       </div>
     );
   }
