5c31a54e231c2e10e325dac8723307d6c900887c
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-09 13:51:49 +0530

    CardList Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -42,13 +43,7 @@ class App extends Component {
           onChange={onSearchChange}
         />
 
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
