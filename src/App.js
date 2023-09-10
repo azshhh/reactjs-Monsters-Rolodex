2b740878bb08e2500a0e07aecf7f27e42449fa4d
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-10 11:37:14 +0530

    Understanding useState() Hook

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
+const App = () => {
+  const [searchField, setSearchField] = useState("");
 
+  // Gives an empty string bcoz in setSearchField we are passing state-value (searchField i,e. '') itself.
+  console.log(searchField);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="search-box"
+        type="search"
+        placeholder="search monsters"
+        onChangeHandler={onSearchChange}
+      />
+      {/* <CardList monsters={filteredMonsters} /> */}
+    </div>
+  );
+};
 
 // class App extends Component {
 //   constructor() {
